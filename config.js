fd2fa67ea5f834387590e0be011e3f9060ef01fe
DominicDev <s0l0estudi4r@gmail.com>
2025-12-12 11:15:32 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'LUCHA REYES';
+export let nameSongs = [
+	"QUE VIVA CHICLAYO", /*1*/
+	"PROPIEDAD PRIVADA", /*2*/
+	"COMO UNA ROSA ROJA", /*3*/
+	"MI ÚLTIMA CANCIÓN", /*4*/
+	"REGRESA", /*5*/
+	"TU VOZ", /*6*/
+	"LA PERLA DEL CHIRA", /*7*/
+	"NADA TE DEBO", /*8*/
+	"ALLÁ ESTARÁS CONMIGO", /*9*/
+	"SECRETO", /*10*/
+	"SIEMPRE TE AMARÉ", /*11*/
+	"ESCLAVITUD", /*12*/
+	"ASÍ LO QUIERES TÚ", /*13*/
+	"PERDÓN POR ADORARTE", /*14*/
+	"JAMÁS IMPEDIRÁS", /*15*/
+	"YO TENGO UNA PENA", /*16*/
+	"UNA CARTA AL CIELO", /*17*/
+	"LA FLOR DE LA CANELA", /*18*/
+	" CONTIGO Y SIN TI", /*19*/
+	"EL PAYANDÉ", /*20*/
+]
